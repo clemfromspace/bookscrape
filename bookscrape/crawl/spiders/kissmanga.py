2cca46415698fa13ad1994c69f0105cb656c57a2
Clement Denoix <flydesigned@gmail.com>
2018-01-28 15:33:55 +0900

    Make the command line handle multiple providers

--- bookscrape/crawl/spiders/kissmanga.py
+++ bookscrape/crawl/spiders/kissmanga.py
@@ -2,113 +2,22 @@
 
 import codecs
 import re
-import cfscrape
 import requests
-from dateutil.parser import parse
+
 from urllib.parse import urljoin
 from Crypto.Hash import SHA256
 from Crypto.Cipher import AES
 from base64 import b64decode
 
-from scrapy import Request
-from scrapy.spiders import CrawlSpider
-
 from bookscrape.exceptions import BookScrapeException
 
-from ..items import BookPageItem
+from .kissmanga_readcomics_base import KissmangaReadcomicsBase
 
 
-class KissmangaSpider(CrawlSpider):
+class KissmangaSpider(KissmangaReadcomicsBase):
     name = 'kissmanga'
     allowed_domains = ['kissmanga.com']
-
-    custom_settings = {
-        'REDIRECT_ENABLED': True,
-        'COOKIES_ENABLED': True,
-        'RETRY_TIMES': 0
-    }
-
-    def __init__(self, book_slug, volume, output_dir, *args, **kwargs):
-        """Init the spider with the start url
-
-        Parameters
-        ----------
-        book_slug: str
-            The slug of the wanted book on the "kissmanga" website
-        volume: str
-            The volume of the wanted book on the "kissmanga" website
-        output_dir: str
-            The path of the directory to place the downloaded files
-
-        """
-
-        super().__init__(*args, **kwargs)
-
-        self.book_slug = book_slug
-        self.volume = int(volume)
-        self.output_dir = output_dir
-
-        self.cloudflare_token = None
-
-    def start_requests(self):
-        """Solve the "Cloudflare" challenge and start the crawling"""
-
-        try:
-            self.cloudflare_token, user_agent = cfscrape.get_tokens(
-                'http://kissmanga.com/',
-                user_agent=self.settings.get('USER_AGENT')
-            )
-        except Exception:
-            raise BookScrapeException(
-                'Unable to bypass "cloudflare" antibot protection'
-            )
-
-        yield Request(
-            url='http://kissmanga.com/Manga/%s' % self.book_slug,
-            cookies=dict(
-                vns_readType1='1',
-                **self.cloudflare_token
-            ),
-            callback=self.parse_volumes_list
-        )
-
-    def parse_volumes_list(self, response):
-        """Parse the volume list and try to find the asked volume"""
-
-        volumes = response.xpath('//table[@class="listing"]//tr[position()>2]')
-
-        if not len(volumes):
-            raise BookScrapeException(
-                'No volumes found for the "%s" slug' % self.book_slug
-            )
-
-        ordered_volumes = []
-
-        for index, volume in enumerate(volumes):
-            volume_id = int(
-                volume.xpath(
-                    './td[1]/a/@href'
-                ).extract_first().split('id=')[-1]
-            )
-            volume_date = parse(volume.xpath('./td[2]/text()').extract_first())
-            ordered_volumes.append((volume_id, volume_date, index,))
-
-        ordered_volumes.reverse()
-
-        try:
-            wanted_volume = ordered_volumes[self.volume-1]
-        except IndexError:
-            raise BookScrapeException(
-                'The %d volume was not found for the "%s" slug' % (
-                    self.volume,
-                    self.book_slug
-                )
-            )
-
-        yield response.follow(
-            '/Manga/%s/v?id=%d' % (self.book_slug, wanted_volume[0]),
-            callback=self.parse_images
-        )
+    url_key = 'Manga'
 
     @staticmethod
     def _decode_images_path(key, obfuscated_path):
@@ -168,8 +77,8 @@ class KissmangaSpider(CrawlSpider):
 
         return decoded_key
 
-    def parse_images(self, response):
-        """Parse the images list of the wanted book volume"""
+    def extract_images_urls(self, response):
+        """Extract list of image urls from the provided response"""
 
         key = self._get_decode_key(response)
         p = re.compile(r'\.push\(wrapKA\("(.+)"\)')
@@ -179,28 +88,10 @@ class KissmangaSpider(CrawlSpider):
             image_urls = [
                 self._decode_images_path(key, image_path)
                 for image_path in obfuscated_image_urls
-            ]
+                ]
         except Exception:
             raise BookScrapeException(
                 'Unable to decrypt the obfuscated image paths :('
             )
 
-        for image_url in image_urls:
-            page_index = image_url.split('/')[-1].replace(
-                '.jpg',
-                ''
-            ).replace('.png', '').replace(
-                '&imgmax=25000',
-                ''
-            ).replace('\x08', '').replace('\x05', '').strip()
-
-            if '-' in page_index:
-                page_index = page_index.split('-')[-1]
-
-            page_index = int(page_index)
-
-            yield BookPageItem(
-                page_index=page_index,
-                volume_index=self.volume,
-                image_urls=[image_url]
-            )
+        return image_urls
--- tests/test_command_line.py
+++ tests/test_command_line.py
@@ -3,12 +3,26 @@
 import unittest
 from unittest import mock
 
-from bookscrape.command_line import crawl, main
+from bookscrape.command_line import _available_spiders, crawl, main
 from bookscrape.crawl.spiders.kissmanga import KissmangaSpider
+from bookscrape.crawl.spiders.readcomiconline import ReadcomiconlineSpider
 
 
 class TestCommandLine(unittest.TestCase):
     """Test case for the command line of the ``bookscrape`` package"""
+
+    def test__available_spiders_should_return_the_list_of_available_spiders(self):
+        """Test that the ``_get_available_spiders`` should return the list of available spiders"""
+
+        available_spiders = {
+            'kissmanga': KissmangaSpider,
+            'readcomiconline': ReadcomiconlineSpider
+        }
+
+        self.assertCountEqual(
+            _available_spiders(),
+            available_spiders
+        )
 
     def test_main_should_raise_if_wrong_arguments_given(self):
         """Test that the ``main`` method should raise if wrong arguments were given"""
@@ -19,26 +33,26 @@ class TestCommandLine(unittest.TestCase):
 
         # This should raise when mandatory arguments are missing
         with self.assertRaises(SystemExit):
-            main(['Akira', '1'])
+            main(['kissmanga', 'Akira', '1'])
 
     @mock.patch('bookscrape.command_line.crawl')
     def test_main_should_call_the_crawl_command(self, crawl):
         """Test that the ``main`` method should call the ``crawl`` method with the corrects args"""
 
         # With only one volume
-        main(['Akira', '1', 'not/a/path'])
-        crawl.assert_called_with('Akira', [1], 'not/a/path', False)
+        main(['kissmanga', 'Akira', '1', 'not/a/path'])
+        crawl.assert_called_with(KissmangaSpider, 'Akira', [1], 'not/a/path', False)
 
         # With two volumes
-        main(['Akira', '1', '2', 'not/a/path'])
-        crawl.assert_called_with('Akira', [1, 2], 'not/a/path', False)
+        main(['kissmanga', 'Akira', '1', '2', 'not/a/path'])
+        crawl.assert_called_with(KissmangaSpider, 'Akira', [1, 2], 'not/a/path', False)
 
     @mock.patch('twisted.internet.reactor.run')
     @mock.patch('scrapy.crawler.CrawlerRunner.crawl')
     def test_crawl_should_call_the_crawl_method_on_the_crawler_runner(self, mocked_crawl, mocked_run):
         """Test that the ``crawl`` method should call the ``CrawlerRunner.crawl``"""
 
-        crawl('Akira', [1], 'not-a-path')
+        crawl(KissmangaSpider, 'Akira', [1], 'not-a-path')
 
         # The crawl method should be called
         mocked_crawl.assert_called_once_with(
@@ -53,7 +67,7 @@ class TestCommandLine(unittest.TestCase):
 
         # If we want to get two volumes, the crawl method should be called twice
         volumes = [1, 2]
-        crawl('Akira', volumes, 'not-a-path')
+        crawl(KissmangaSpider, 'Akira', volumes, 'not-a-path')
 
         calls = [
             mock.call(
